e2096b4bf6058193364bfba0e4b368de3cc6b452
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2022-12-14 14:12:18 +0200

    test

--- front/functions/bot/bot.js
+++ front/functions/bot/bot.js
@@ -8,66 +8,66 @@ dotenv.config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-function start() {
-  // bot.start((ctx) => ctx.reply('Welcome'))
-  // bot.help((ctx) => ctx.reply('Send me a sticker'))
-  // bot.on('sticker', (ctx) => ctx.reply('👍'))
-  // bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+// function start() {
+// bot.start((ctx) => ctx.reply('Welcome'))
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.on('sticker', (ctx) => ctx.reply('👍'))
+// bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
-  // bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-  // bot.command('hipster', Telegraf.reply('λ'))
+// bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+// bot.command('hipster', Telegraf.reply('λ'))
 
-  // bot.on('text', (ctx) => ctx.reply(ctx.message.text))
+// bot.on('text', (ctx) => ctx.reply(ctx.message.text))
 
-  // bot.action('btn_action', (ctx) => {})
+// bot.action('btn_action', (ctx) => {})
 
-  // bot.on('text', async (ctx) => {
-  //   console.log(ctx)
+// bot.on('text', async (ctx) => {
+//   console.log(ctx)
 
-  //   // await makeResponse(bot, {
-  //   //   firstName: ctx.message.from.first_name,
-  //   //   lastName: ctx.message.from.last_name,
-  //   //   username: ctx.message.from.username,
-  //   //   chatId: ctx.message.chat.id,
-  //   //   command: ctx.message.text,
-  //   //   ctx,
-  //   // })
-  // })
+//   // await makeResponse(bot, {
+//   //   firstName: ctx.message.from.first_name,
+//   //   lastName: ctx.message.from.last_name,
+//   //   username: ctx.message.from.username,
+//   //   chatId: ctx.message.chat.id,
+//   //   command: ctx.message.text,
+//   //   ctx,
+//   // })
+// })
 
-  // bot.help(async (ctx) => {
-  //   const firstName = ctx.message.from.first_name
-  //   const lastName = ctx.message.from.last_name
-  //   const username = ctx.message.from.username
-  //   const command = ctx.message.text
-  //   const chatId = ctx.message.chat.id
+// bot.help(async (ctx) => {
+//   const firstName = ctx.message.from.first_name
+//   const lastName = ctx.message.from.last_name
+//   const username = ctx.message.from.username
+//   const command = ctx.message.text
+//   const chatId = ctx.message.chat.id
 
-  //   const user = await handleUser(bot)
-  // })
+//   const user = await handleUser(bot)
+// })
 
-  // bot.command('help', (ctx) => {
-  //   ctx.replyWithHTML('HELP')
-  // })
+// bot.command('help', (ctx) => {
+//   ctx.replyWithHTML('HELP')
+// })
 
-  bot.start(async (ctx) => {
-    console.log('Received /start command')
-    try {
-      return await ctx.reply('Welcome')
-    } catch (e) {
-      console.error('error in start action:', e)
-      return await ctx.reply('Error occured')
-    }
-  })
+bot.start(async (ctx) => {
+  console.log('Received /start command')
+  try {
+    return await ctx.reply('Welcome')
+  } catch (e) {
+    console.error('error in start action:', e)
+    return await ctx.reply('Error occured')
+  }
+})
 
-  console.log('✅ The bot is configured and working correctly')
-}
+//   console.log('✅ The bot is configured and working correctly')
+// }
 
-start()
+// start()
 
-bot.launch()
+// bot.launch()
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+// // Enable graceful stop
+// process.once('SIGINT', () => bot.stop('SIGINT'))
+// process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 exports.handler = async (event) => {
