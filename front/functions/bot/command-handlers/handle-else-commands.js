c9b9fc02404bee73ec426697d42864c0e44642bd
Denys Kozubskyi <denys.kozubskyi@gmail.com>
2022-12-18 00:25:02 +0200

    update creator commands

--- front/functions/bot/command-handlers/handle-else-commands.js
+++ front/functions/bot/command-handlers/handle-else-commands.js
@@ -1,110 +1,95 @@
-const axios = require('axios');
-const { DB_BASE_URL } = process.env;
-const { SWEET_CHAT_ID } = require('../helpers/chat-ids');
+const axios = require('axios')
+const { DB_BASE_URL } = process.env
+const { SWEET_CHAT_ID } = require('../helpers/chat-ids')
 
 function separateFirstWord(msg) {
-  const msgArr = msg.split(' ');
-  const cmd = msgArr[0];
-  const text = msgArr.slice(1).join(' ');
+  const msgArr = msg.split(' ')
+  const cmd = msgArr[0]
+  const text = msgArr.slice(1).join(' ')
 
-  return [cmd, text];
+  return [cmd, text]
 }
 
 async function handleCreatorCommands(ctx, value) {
-  let reply = '';
+  let reply = ''
 
-  const [command, rest] = separateFirstWord(value);
+  const [command, rest] = separateFirstWord(value)
 
   if (command === 'add') {
-    const { data } = await axios.post(`${DB_BASE_URL}/compliments`, { text: rest });
+    const { data } = await axios.post(`${DB_BASE_URL}/compliments`, { text: rest })
 
-    reply = `✅ Компліментик додано в базу даних: ${JSON.stringify(data)}`;
+    reply = `✅ Компліментик додано в базу даних: ${JSON.stringify(data)}`
   } else if (command === 'show') {
-    const { data } = await axios.get(`${DB_BASE_URL}/compliments/${rest}`);
+    const { data } = await axios.get(`${DB_BASE_URL}/compliments/${rest}`)
 
-    reply = JSON.stringify(data);
+    reply = JSON.stringify(data)
   } else if (command === 'del') {
-    const [key, idOrText] = separateFirstWord(rest);
+    const [key, idOrText] = separateFirstWord(rest)
 
-    const { data } = await axios.delete(`${DB_BASE_URL}/compliments/${key}/${idOrText}`);
+    const { data } = await axios.delete(`${DB_BASE_URL}/compliments/${key}/${idOrText}`)
 
     if (data) {
-      reply = `✅ Компліментик успішно видалено з бази даних: ${JSON.stringify(data)}`;
+      reply = `✅ Компліментик успішно видалено з бази даних: ${JSON.stringify(data)}`
     } else {
-      reply = 'ℹ️ Такого компліментика і не було у базі даних';
+      reply = 'ℹ️ Такого компліментика і не було у базі даних'
     }
   } else if (command === 'upd') {
-    const [id, fieldAndNewValue] = separateFirstWord(rest);
-    const [field, newValue] = separateFirstWord(fieldAndNewValue);
+    const [collection, idFieldAndNewValue] = separateFirstWord(rest)
+    const [id, fieldAndNewValue] = separateFirstWord(idFieldAndNewValue)
+    const [field, newValue] = separateFirstWord(fieldAndNewValue)
 
-    const { data } = await axios.patch(`${DB_BASE_URL}/compliments/${id}`, {
+    const { data } = await axios.patch(`${DB_BASE_URL}/${collection}s/${id}`, {
       [field]: JSON.parse(newValue),
-    });
+    })
 
     if (data) {
-      reply = `✅ Компліментик успішно оновлено у базі даних: ${JSON.stringify(data)}`;
+      reply = `✅ Компліментик успішно оновлено у базі даних: ${JSON.stringify(data)}`
     } else {
-      reply = `⚠️ Компліментика з id ${id} немає в базі даних`;
+      reply = `⚠️ Компліментика з id ${id} немає в базі даних`
     }
   } else if (command === 'mlr') {
-    await ctx.telegram.sendMessage(SWEET_CHAT_ID, rest);
+    await ctx.telegram.sendMessage(SWEET_CHAT_ID, rest)
 
-    reply = '✅ Повідомлення Лєнусічкє відправлено';
+    reply = '✅ Повідомлення Лєнусічкє відправлено'
   } else if (command === 'msg') {
-    const [name, surnameAndText] = separateFirstWord(rest);
-    const [surname, text] = separateFirstWord(surnameAndText);
+    const [name, surnameAndText] = separateFirstWord(rest)
+    const [surname, text] = separateFirstWord(surnameAndText)
 
-    const { data } = await axios.get(`${DB_BASE_URL}/users/${name}/${surname}`);
+    const { data } = await axios.get(`${DB_BASE_URL}/users/${name}/${surname}`)
 
     if (data) {
-      await ctx.telegram.sendMessage(data.chatId, text);
-      reply = '✅ Повідомлення відправлено';
+      await ctx.telegram.sendMessage(data.chatId, text)
+      reply = '✅ Повідомлення відправлено'
     } else {
-      reply = '⚠️ Такого користувача немає у базі даних';
+      reply = '⚠️ Такого користувача немає у базі даних'
     }
-  } else if (value === 'delallcompliments') {
-    await axios.delete(`${DB_BASE_URL}/compliments`);
+  } else if (value === 'users' || value === 'compliments') {
+    const { data } = await axios.get(`${DB_BASE_URL}/${value}`)
 
-    reply = '✅ Всі компліментики видалено з бази даних';
-  } else if (value === '/users') {
-    const { data } = await axios.get(`${DB_BASE_URL}/users`);
+    reply = JSON.stringify(data.map.slice(rest * 20 - 20, rest * 20))
+  } else if (value === '/usersq' || value === '/complimentsq') {
+    const { data } = await axios.get(`${DB_BASE_URL}/${value.slice(1, value.length - 1)}`)
 
-    reply = JSON.stringify(data);
-  } else if (value === '/compliments') {
-    const { data } = await axios.get(`${DB_BASE_URL}/compliments`);
-
-    reply = JSON.stringify(data.map(({ _id, text }) => `${_id} ${text}`));
-  } else if (value === '/usersq') {
-    const { data } = await axios.get(`${DB_BASE_URL}/users`);
-
-    reply = `${data.length}`;
-  } else if (value === '/complimentsq') {
-    const { data } = await axios.get(`${DB_BASE_URL}/compliments`);
-
-    reply = `${data.length}`;
-  } else if (value === '/sendingsreset') {
-    await axios.patch(`${DB_BASE_URL}/compliments/sendings/reset`);
-
-    reply = '✅ Поле sendings усіх повідомлень успішно скинуто до 0';
+    reply = `${data.length}`
   } else if (value === '/test') {
-    reply = '✅';
+    reply = '✅'
   } else {
-    reply = '⚠️ Некоректна команда';
+    reply = '⚠️ Некоректна команда'
   }
 
-  return reply;
+  return reply
 }
 
 module.exports = async function handleElseCommands(ctx, status, value) {
-  let reply = '';
+  let reply = ''
 
   if (status === 'creator') {
-    reply = await handleCreatorCommands(ctx, value);
+    reply = await handleCreatorCommands(ctx, value)
   } else if (status === 'sweet') {
-    reply = 'Я передам Денису те, що ти написала) 😘';
+    reply = 'Я передам Денису те, що ти написала) 😘'
   } else {
-    reply = 'Я передам Денису це повідомлення 😉';
+    reply = 'Я передам Денису це повідомлення 😉'
   }
 
-  return reply;
-};
+  return reply
+}
